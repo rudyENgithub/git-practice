a09a21aedf4bae51a729430125c2de49e5ffa9d4
Rudy Sorto <rudy.eli.ayala@gmail.com>
2018-09-20 11:24:24 -0600

    implementacion inicial

---
+++ src/App.java
@@ -0,0 +1,18 @@
+package org.rudysorto.gitbook;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		  System.out.println("Hola Java");
+    
+    }
+   
+    
+
+}
